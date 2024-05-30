8c70fc42ca1c8b8ce596f4176e4e1af4f740b24e
utkarsh <khareu550@gmail.com>
2024-05-30 22:34:04 +0530

    contact page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,29 +6,29 @@ function Header() {
       <nav className="navbar-expand-lg fixed top-0 z-50 flex w-full flex-wrap items-center justify-between bg-white px-2 py-3 shadow">
         <div className="container mx-auto flex flex-wrap items-center justify-between px-4">
           <div className="relative flex w-full justify-between lg:static lg:block lg:w-auto lg:justify-start">
-            <Link className="text-slate-700 mr-4 inline-block whitespace-nowrap py-2 text-sm font-bold uppercase leading-relaxed" href="#pablo">Notus NextJS</Link>
+            <Link className="text-slate-700 mr-4 inline-block whitespace-nowrap py-2 text-sm font-bold uppercase leading-relaxed" href="/">Notus NextJS</Link>
           </div>
           <div className="hidden flex-grow items-center bg-white lg:flex lg:bg-opacity-0 lg:shadow-none" id="example-navbar-warning">
 
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
               <li className="flex items-center">
-                <a className="text-slate-700 hover:text-slate-500 text-sm font-bold uppercase px-3 py-4 lg:py-2 flex items-center" href="#pablo">
-                  <i className="fas fa-home text-lg leading-lg text-slate-400"></i><span className="inline-block ml-2">Home</span>
+                <Link className="text-slate-700 hover:text-slate-500 text-sm font-bold uppercase px-3 py-4 lg:py-2 flex items-center" href="/">
+                  <span className="inline-block ml-2">Home</span>
+                </Link>
+              </li>
+              <li className="flex items-center">
+                <a className="text-slate-700 hover:text-slate-500 text-sm font-bold uppercase px-3 py-4 lg:py-2 flex items-center" href="/technology">
+                 <span className="inline-block ml-2">Technology</span>
                 </a>
               </li>
               <li className="flex items-center">
-                <a className="text-slate-700 hover:text-slate-500 text-sm font-bold uppercase px-3 py-4 lg:py-2 flex items-center" href="#pablo">
-                  <i className="fas fa-sitemap text-lg leading-lg text-slate-400"></i><span className="inline-block ml-2">Technology</span>
+                <a className="text-slate-700 hover:text-slate-500 text-sm font-bold uppercase px-3 py-4 lg:py-2 flex items-center" href="/career">
+                  <span className="inline-block ml-2">Carrer</span>
                 </a>
               </li>
               <li className="flex items-center">
-                <a className="text-slate-700 hover:text-slate-500 text-sm font-bold uppercase px-3 py-4 lg:py-2 flex items-center" href="#pablo">
-                  <i className="fas fa-users text-lg leading-lg text-slate-400"></i><span className="inline-block ml-2">Carrer</span>
-                </a>
-              </li>
-              <li className="flex items-center">
-                <a className="text-slate-700 hover:text-slate-500 text-sm font-bold uppercase px-3 py-4 lg:py-2 flex items-center" href="#pablo">
-                  <i className="fas fa-file-alt text-lg leading-lg text-slate-400"></i><span className="inline-block ml-2">Contact</span>
+                <a className="text-slate-700 hover:text-slate-500 text-sm font-bold uppercase px-3 py-4 lg:py-2 flex items-center" href="/contact">
+                 <span className="inline-block ml-2">Contact</span>
                 </a>
               </li>
             </ul>
